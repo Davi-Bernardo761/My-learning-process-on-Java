39cc50f7da3a725756cda158505f9f0a6da8a7f7
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-14 20:28:55 -0300

    Update SphereAreaVolumeAndCircumference.java

--- SphereAreaVolumeAndCircumference.java
+++ SphereAreaVolumeAndCircumference.java
@@ -1,3 +1,4 @@
+//This program asks the user the value of a radius, whit this value calculates the lenght of the circumference, the area of the circle, the area of the sphere and it's volume. 
 import java.util.Scanner;
 public class SphereAreaVolumeAndCircumference {
     public static void main (String[] args){
