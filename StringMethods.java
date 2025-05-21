a1b0e022ef5c348face17829ea532f528a2f5f30
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-21 18:22:37 -0300

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,17 @@
+public class StringMethods {
+    public static void main (String[] args){
+        String name = "Thomas";
+        int length = name.length();
+        System.out.println(length);
+        char letter = name.charAt(0);
+        System.out.println(letter);
+        int index = name.indexOf(" ");
+        System.out.println(index);
+        int Lastindex = name.lastIndexOf("o");
+
+        name = name.toUpperCase();
+        name = name.toLowerCase();
+        name = name.trim();
+        name = name.replace("o","a");
+    }
+}
