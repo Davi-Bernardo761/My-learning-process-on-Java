ac88cb7cc07c1e3fa0f4ee9615418e4c2feffd65
Davi-Bernardo761 <davibernardotrabalho@gmail.com>
2025-05-21 20:48:01 -0300

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+//This program demonstrates a simple use of the ternary operator.
+public class TernaryOperator {
+    public static void main (String[] args){
+        int height = 10;
+        String istall = (height >=10)? "The building is tall.": "The building isn't tall.";
+        System.out.println(istall);
+    }
+}
